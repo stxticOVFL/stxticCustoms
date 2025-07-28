f10c5d2b5d3b43501e414c5a10ff610d6f710956
st×tic <rmgrichdeveloper@gmail.com>
2025-07-27 23:31:16 -0500

    yeah (gitgnore version)

--- UnityProject/Assets/Editor/assetbundle.cs
+++ UnityProject/Assets/Editor/assetbundle.cs
@@ -12,7 +12,7 @@ public class CreateAssetBundles
             Directory.CreateDirectory(assetBundleDirectory);
         }
         BuildPipeline.BuildAssetBundles(assetBundleDirectory,
-                                        BuildAssetBundleOptions.StrictMode,
+                                        BuildAssetBundleOptions.ChunkBasedCompression,
                                         BuildTarget.StandaloneWindows);
     }
 }
